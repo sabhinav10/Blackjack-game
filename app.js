afb9892c6cfe1dd04ee16edbd542b5d44ca28faa
sabhinav10 <sabhinav654@gmail.com>
2021-11-09 12:34:03 +0530

    initial structure

---
+++ app.js
@@ -0,0 +1,19 @@
+let firstCard = 2;
+let secondCard = 8;
+let sum = firstCard + secondCard + 4;
+let hasBlackJack = false;
+let isAlive = true;
+//console.log(sum);
+let message = "";
+if(sum <= 20){
+    message = "Do you want to draw a new card? 😃";
+}
+else if(sum === 21){
+  message = "Wohoo!!! You have got Blackjack 🎉🎉";
+  hasBlackJack = true;
+}
+else{
+    message = "You're out of the game! 😥😥😥";
+    isAlive = false;
+}
+console.log(message);
